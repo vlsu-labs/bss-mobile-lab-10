8f0f8470162d6ba499e0b7de2eb1e50eb2163136
w1sp <semen2002bs@gmail.com>
2023-12-24 23:17:58 +0300

    third eaxmple

--- App.tsx
+++ App.tsx
@@ -1,49 +1,48 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
 class App extends Component {
  constructor(props) {
  super(props);
- this.state = { pressing: false };
- }
- _onPressIn = () => {
- this.setState({ pressing: true });
+ this.state = {
+ data: [
+ {key: 'a'},
+ {key: 'b'},
+ {key: 'c'},
+ {key: 'd'},
+ {key: 'a longer example'},
+ {key: 'e'},
+ {key: 'f'},
+ {key: 'g'},
+ {key: 'h'},
+ {key: 'i'},
+ {key: 'j'},
+ {key: 'k'},
+ {key: 'l'},
+ {key: 'm'},
+ {key: 'n'},
+ {key: 'o'},
+ {key: 'p'},
+ ],
  };
- _onPressOut = () => {
- this.setState({ pressing: false });
+ }
+ _renderItem = data => {
+ return <Text style={styles.row}>{data.item.key}</Text>;
  };
  render() {
  return (
  <View style={styles.container}>
- <TouchableHighlight
- onPressIn={this._onPressIn}
- onPressOut={this._onPressOut}
- style={styles.touchable}
- >
- <View style={styles.button}>
- <Text style={styles.welcome}>
- {this.state.pressing ? "НАЖАТО!" : "НАЖМИ!"}
- </Text>
- </View>
- </TouchableHighlight>
+ <FlatList data={this.state.data} renderItem={this._renderItem} />
  </View>
  );
  }
 }
 const styles = StyleSheet.create({
-container: {
-flex: 1,
-justifyContent: "center",
-alignItems: "center",
-backgroundColor: "#F5FCFF"
+ container: {
+ flex: 1,
+ justifyContent: 'center',
+ alignItems: 'center',
+ backgroundColor: '#F5FCFF',
  },
-welcome: { fontSize: 20, textAlign: "center", margin: 10, color: "#FFFFFF" },
-touchable: { borderRadius: 100 },
-button: {
-backgroundColor: "#FF00FF",
-borderRadius: 100,
-height: 200,
-width: 200,
-justifyContent: "center"
- }
+ row: {fontSize: 24, padding: 42, borderWidth: 1, borderColor: '#DDDDDD'},
 });
 export default App;
