2761316a0dbb2d1eb4d172a6543167ff8a5a3218
w1sp <semen2002bs@gmail.com>
2023-12-24 23:15:53 +0300

    second example

--- App.tsx
+++ App.tsx
@@ -1,15 +1,49 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-export default class App extends React.Component {
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+class App extends Component {
+ constructor(props) {
+ super(props);
+ this.state = { pressing: false };
+ }
+ _onPressIn = () => {
+ this.setState({ pressing: true });
+ };
+ _onPressOut = () => {
+ this.setState({ pressing: false });
+ };
  render() {
- return ( <Text>Hello, world!</Text> );
+ return (
+ <View style={styles.container}>
+ <TouchableHighlight
+ onPressIn={this._onPressIn}
+ onPressOut={this._onPressOut}
+ style={styles.touchable}
+ >
+ <View style={styles.button}>
+ <Text style={styles.welcome}>
+ {this.state.pressing ? "НАЖАТО!" : "НАЖМИ!"}
+ </Text>
+ </View>
+ </TouchableHighlight>
+ </View>
+ );
  }
 }
 const styles = StyleSheet.create({
- container: {
- flex: 1,
- backgroundColor: "#fff",
- alignItems: "center",
- justifyContent: "center"
+container: {
+flex: 1,
+justifyContent: "center",
+alignItems: "center",
+backgroundColor: "#F5FCFF"
+ },
+welcome: { fontSize: 20, textAlign: "center", margin: 10, color: "#FFFFFF" },
+touchable: { borderRadius: 100 },
+button: {
+backgroundColor: "#FF00FF",
+borderRadius: 100,
+height: 200,
+width: 200,
+justifyContent: "center"
  }
 });
+export default App;
